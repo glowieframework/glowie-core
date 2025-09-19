a04d1ba3de43a2c8d28766da11dc0273832e8595
Gabriel Oliveira <gabriel.o.silva10@gmail.com>
2025-09-19 10:13:08 -0300

    Fix Request isMobile return

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -423,7 +423,7 @@ class Request implements JsonSerializable
      */
     public function isMobile()
     {
-        return preg_match("/(android|webos|avantgo|iphone|ipad|ipod|blackberry|iemobile|bolt|boost|cricket|docomo|fone|hiptop|mini|opera mini|kitkat|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $this->getHeader('User-Agent', ''));
+        return (bool)preg_match("/(android|webos|avantgo|iphone|ipad|ipod|blackberry|iemobile|bolt|boost|cricket|docomo|fone|hiptop|mini|opera mini|kitkat|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $this->getHeader('User-Agent', ''));
     }
 
     /**
